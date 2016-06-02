e240686baeaa6d5d7a96b0de8d52b9b349aca9cf
fenom <fenom23@hotmail.com>
2016-06-02 02:31:40 +0000

    configs

--- config/app.php
+++ config/app.php
@@ -155,6 +155,9 @@ return [
         DataReaper\Providers\AuthServiceProvider::class,
         DataReaper\Providers\EventServiceProvider::class,
         DataReaper\Providers\RouteServiceProvider::class,
+        DataReaper\Providers\ComposerServiceProvider::class,
+        Jenssegers\Mongodb\MongodbServiceProvider::class,
+        Collective\Html\HtmlServiceProvider::class,
 
     ],
 
@@ -201,6 +204,9 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Moloquent' => Jenssegers\Mongodb\Eloquent\Model::class,
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
 
     ],
 
