574ed3ecd429d4d4569ffafec77ddac88d950694
fenom <fenom23@hotmail.com>
2016-06-23 04:09:06 +0000

    can take timestamps now

--- app/Repositories/MetaRepository.php
+++ app/Repositories/MetaRepository.php
@@ -11,7 +11,7 @@ class MetaRepository
      *
      * @var array
      */
-    protected $query = ['added' => [0], 'time' => ['00:00:00', '23:59:59'], 'rank' => [0], 'region' => ['Americas', 'Europe'], 'format' => ['Standard'], 'mode' => ['ranked']];
+    protected $query = ['added' => [0], 'time' => ['today pdt', 'tomorrow-1sec pdt'], 'rank' => [0], 'region' => ['Americas', 'Europe'], 'format' => ['Standard'], 'mode' => ['ranked']];
 
     /**
      * Get meta.
@@ -24,9 +24,9 @@ class MetaRepository
         $datareaper = \DB::getMongoDB();
         $query = array_filter($query) + $this->query;
         $query = array_map(function($value) {return (array) $value;}, $query);
-        $query['time'][0] = strtotime($query['time'][0]);
+        is_numeric($query['time'][0]) or $query['time'][0] = strtotime($query['time'][0]);
         $query['time'][0] = idate('H', $query['time'][0]) * 60 * 60 + idate('i', $query['time'][0]) * 60 + idate('s', $query['time'][0]);
-        $query['time'][1] = strtotime($query['time'][1]);
+        is_numeric($query['time'][1]) or $query['time'][1] = strtotime($query['time'][1]);
         $query['time'][1] = idate('H', $query['time'][1]) * 60 * 60 + idate('i', $query['time'][1]) * 60 + idate('s', $query['time'][1]);
         sort($query['added']);
         sort($query['rank']);
@@ -35,9 +35,9 @@ class MetaRepository
                   'format' => ['$in' => $query['format']],
                   'rank' => ['$gte' => $query['rank'][0] = (int) $query['rank'][0], '$lte' => $query['rank'][1] = isset($query['rank'][1]) ? (int) $query['rank'][1] : 25],
                   //'legend' => !$query['rank'][0] ? ['$gte' => $legend[0]] + (isset($legend[1]) ? ['$lte' => $legend[1]] : []) : null,
-                  'added' => ['$gte' => new \MongoDate($query['added'][0] = $query['added'][0] ? max(strtotime($query['added'][0]), strtotime('2016-05-01 PDT')) : time() - 60 * 60 * 24 - 30 * 60),
-                              '$lte' => new \MongoDate($query['added'][1] = isset($query['added'][1]) ? strtotime($query['added'][1]) : time() - 30 * 60)],
-                  ($query['time'][0] > $query['time'][1] ? '$or' : '$and') => [['time' => ['$gte' => $query['time'][0]]], ['time' => ['$lte' => $query['time'][1]]]]
+                  'added' => ['$gte' => new \MongoDate($query['added'][0] = $query['added'][0] ? max(is_numeric($query['added'][0]) ? $query['added'][0] : strtotime($query['added'][0]), strtotime('2016-05-01pdt')) : time() - 60 * 60 * 24 - 30 * 60),
+                              '$lte' => new \MongoDate($query['added'][1] = isset($query['added'][1]) ? is_numeric($query['added'][1]) ? $query['added'][1] : strtotime($query['added'][1]) : time() - 30 * 60)],
+                  $query['time'][0] > $query['time'][1] ? '$or' : '$and' => [['time' => ['$gte' => $query['time'][0]]], ['time' => ['$lte' => $query['time'][1]]]]
                  ];
         $group = ['_id' => ['class' => '$opponent', 'archetype' => '$opponent_deck'], 'count' => ['$sum' => 1]];
         $sort = ['_id.class' => 1, 'count' => -1];
